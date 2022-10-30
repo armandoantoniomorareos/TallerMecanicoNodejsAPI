474a119e11be0163b76dddee48241746c016d3bd
armandoantoniomorareos <arm_antonio_mr@hotmail.com>
2022-10-30 10:34:28 -0600

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+const config ={
+    db:{
+        host : "localhost",
+        user:"armando",
+        password:"12345678",
+        database:"taller_mecanico"
+    },
+    listPerPage: 10
+};
+module.exports = config
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { getUser } = require('./services/login');
+const user = require("./routes/user");
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use("/user", user);
+
+app.listen(
+    PORT,
+    () => console.log(`It´s alive on http://localhost:${PORT}`)
+);
+
+app.get('/tshirt', (req, res) =>{
+    res.status(200).send(
+        {
+            tshirt:'tshirt',
+            size : 'large'
+        }
+    )
+});
+
+
+app.post('/tshirt/:id', (req, res)=>{
+    const {id} = req.params;
+    const {logo} = req.body;
+    if(!logo){
+        res.status(418).send({message: 'We need a logo!'})
+    } 
+
+    res.send({
+        tshirt:`with your ${logo} and ID if ${id}`,
+    });
+});
